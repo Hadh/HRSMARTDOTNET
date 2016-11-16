a657acb619b660a17136c3141abf41fd464ff4ec
alaa ksontini <alaa.ksontini@esprit.tn>
2016-11-16 11:28:49 +0100

    First Commit with good ignorefile

---
+++ HRSmart.Service/Business/ServiceQuestion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRSmart.data.Infrastructure;
+using HRSmart.Domain.Entities;
+using HRSmart.ServicePattern;
+
+namespace HRSmart.Service.Business
+{
+    public class ServiceQuestion : MyServiceGeneric<question>, IServiceQuestion
+    {
+        private static IDataBaseFactory dbfac = new DataBaseFactory();
+        private static IUnitOfWork itw = new UnitOfWork(dbfac);
+
+        public ServiceQuestion() : base(itw)
+        {
+        }
+    }
+}
---
+++ HRSmart/Controllers/QuizController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRSmart.Domain.Entities;
+using HRSmart.Service.Business;
+
+namespace HRSmart.Controllers
+{
+    public class QuizController : Controller
+    {
+        private IServiceQuiz sq = new ServiceQuiz();
+        private IServiceQuestion squ = new ServiceQuestion();
+        
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            IDictionary<string,int> assoc = new Dictionary<string,int>();
+            IEnumerable<question> questions = squ.GetMany();
+            foreach (question question in questions)
+            {
+                assoc[question.body] = question.quizs.Count;
+            }
+            ViewBag.Data = assoc; 
+            return View();
+        }
+    }
+}
