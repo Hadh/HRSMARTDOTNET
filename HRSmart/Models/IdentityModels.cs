aa83526abf025e13648a711e1e1567ad5583e03d
hdmi <soccerstar.laouini@gmail.com>
2016-11-16 13:14:28 +0100

    random

--- HRSmart/Models/IdentityModels.cs
+++ HRSmart/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace HRSmart.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("mysqlpiContext", throwIfV1Schema: false)
         {
         }
 
