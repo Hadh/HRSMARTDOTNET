fb992cba54efb2b3ddbee12443cb207d7ace3e9f
alaa ksontini <alaa.ksontini@esprit.tn>
2016-11-17 20:21:17 +0100

    Testing Api.ai bot working fine with smallTalk domain.

---
+++ HRSmart/Utility/ApiAiUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ApiAiSDK;
+
+namespace HRSmart.Utility
+{
+    public class ApiAiUtility
+    {
+        private ApiAi ai;
+
+        public ApiAiUtility()
+        {
+            var config = new AIConfiguration("2b4219f782904d6a996efcc54b5742fe", SupportedLanguage.English);
+            ai = new ApiAi(config);
+        }
+    }
+}
