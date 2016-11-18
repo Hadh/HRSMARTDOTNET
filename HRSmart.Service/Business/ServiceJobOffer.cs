225681499754c1b90296797b7cca2ad9f2b9609d
heiyuki <khaled.romdhane@esprit.tn>
2016-11-18 20:21:17 +0100

    JobOffers/JobSkills/Skills/Rewards

---
+++ HRSmart.Service/Business/ServiceJobOffer.cs
@@ -0,0 +1,79 @@
+﻿using HRSmart.data.Infrastructure;
+using HRSmart.Domain.Entities;
+using HRSmart.ServicePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSmart.Service.Business
+{
+    public class ServiceJobOffer : MyServiceGeneric<joboffer>, IServiceJobOffer
+    {
+        private static IDataBaseFactory dbfac = new DataBaseFactory();
+        private static IUnitOfWork utw = new UnitOfWork(dbfac);
+        public ServiceJobOffer() : base(utw)
+        {
+
+        }
+        public float getAverageSalary()
+        {
+            List<joboffer> jobs = this.GetMany().ToList();
+            float average = 0;
+            foreach (var j in jobs)
+            {
+                average += j.salary;
+            }
+            return average / jobs.Count;
+        }
+        public float getPercentageAvailableJobs()
+        {
+            List<joboffer> jobs = this.GetMany().ToList();
+            float counter = 0;
+            foreach (var j in jobs)
+            {
+                if (j.active)
+                {
+                    counter++;
+                }
+            }
+            return (counter / jobs.Count) * 100;
+        }
+        public int getNumberOfPostulations(int id)
+        {
+            joboffer job = this.GetById(id);
+            int counter = 0;
+            foreach (var r in job.rewards)
+            {
+                counter += r.postulations.Count;
+            }
+            return counter;
+        }
+
+        public List<joboffer> getMostPopularJobs(int number)
+        {
+            List<joboffer> jobs = this.GetMany().ToList();
+            List<joboffer> ret = new List<joboffer>();
+            int counter = 0;
+            while (counter < number)
+            {
+                joboffer max = jobs.ElementAt(0);
+                for (int i = 0; i < jobs.Count - (counter); i++)
+                {
+                    Console.WriteLine(jobs.ElementAt(i));
+                    if (this.getNumberOfPostulations(jobs.ElementAt(i).id) > this.getNumberOfPostulations(max.id))
+                    {
+                        max = jobs.ElementAt(i);
+
+                    }
+                }
+                ret.Add(max);
+                jobs.Remove(max);
+                counter++;
+                if (counter == jobs.Count) { break; }
+            }
+            return ret;
+        }
+    }
+}
---
+++ HRSmart/Controllers/JobOfferController.cs
@@ -0,0 +1,58 @@
+﻿using HRSmart.Domain.Entities;
+using HRSmart.Service.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRSmart.Controllers
+{
+    public class JobOfferController : Controller
+    {
+        private ServiceJobOffer jobService = new ServiceJobOffer();
+        // GET: JobOffer
+        public ActionResult Index()
+        {
+            List<joboffer> jobs = jobService.GetMany().ToList();
+            ViewBag.jobs = jobs;
+            return View();
+        }
+
+        // GET: JobOffer/Details/5
+        public ActionResult Details(int id)
+        {
+            joboffer job = jobService.GetById(id);
+            ViewBag.job = job;
+            ViewBag.numPostulations = jobService.getNumberOfPostulations(job.id);
+            return View();
+        }
+
+        public ActionResult Switch(int id)
+        {
+            joboffer job = jobService.GetById(id);
+            if (job.active == true)
+            {
+                job.active = false;
+            }
+            else
+            {
+                job.active = true;
+
+            }
+            jobService.commit();
+            return RedirectToAction("Index", "JobOffer");
+        }
+        public ActionResult JobsAnalytics()
+        {
+            ViewBag.available = jobService.getPercentageAvailableJobs();
+            ViewBag.popular = jobService.getMostPopularJobs(5);
+            return View();
+        }
+        public ActionResult JobAnalytics()
+        {
+            ViewBag.available = jobService.getPercentageAvailableJobs();
+            return View();
+        }
+    }
+}
